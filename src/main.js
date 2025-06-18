e5fc311eb51d0bb09accc170ac6da09a34c2cccf
Alex <aluciencozy22@gmail.com>
2025-06-18 13:39:48 -0400

    Implement hero section layout with image and button

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/header.css';
+import '../styles/about.css';
 import '../styles/hero.css';
 import '../styles/utils.css';
