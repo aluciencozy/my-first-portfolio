d46f0af61f1bd6bcb077cf55f1d7c93419b7f498
Alex <aluciencozy22@gmail.com>
2025-06-19 15:51:52 -0400

    finished first draft of the projects section

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
 import '../styles/header.css';
-import '../styles/about.css';
 import '../styles/hero.css';
+import '../styles/about.css';
+import '../styles/projects.css';
 import '../styles/utils.css';
